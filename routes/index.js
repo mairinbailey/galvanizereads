bfafb998ff532c8e941b8236583d88a5b74c7dcd
Mairin Bailey <mairin.bailey@gmail.com>
2016-07-12 12:36:53 -0600

    add list books functionality

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var knex = require('../db/knex');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/books', function(req, res, next) {
+  knex('book').select().then(function(books){
+  res.render('books', {books: books});
+  });
+});
+
+module.exports = router;
